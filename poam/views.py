bc88fed7e08eec817417c388b78cd7a0a35ff26a
ryan <hensojr@gmail.com>
2018-01-12 10:51:00 -0700

    Added functionality to STIG upload

--- poam/views.py
+++ poam/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from xml.etree import ElementTree
+from django.db import IntegrityError
 
 from .forms import *
 from .models import *
@@ -14,7 +15,6 @@ class UploadArtifactView(LoginRequiredMixin, generic.CreateView):
     model = Weakness
     form_class = DocumentForm
     def form_valid(self, form):
-        print(form.cleaned_data)
         data = form.cleaned_data['file']
         source_id = form.cleaned_data['source_identifying_event']
         source_tool = form.cleaned_data['source_identifying_tool']
@@ -22,39 +22,49 @@ class UploadArtifactView(LoginRequiredMixin, generic.CreateView):
         poc = form.cleaned_data['point_of_contact']
         file_type = form.cleaned_data['file_type']
 
-
         # read and parse the file, create a Python dictionary `data_dict` from it
         # start loop here for each Vuln_num in xml upload get Rule_title, Vuln_discuss, Comments, IA_Controls, Check_Content, Severity, Stigid
         if file_type == 'stig_checklist_file':
             # Need check and message to make sure Trey or more than likely zac doesn't do something dumb
             tree = ElementTree.parse(data)
             root = tree.getroot()
-            for vuln in root.findall('.//VULN'):
-                status = vuln.find('STATUS').text
-                comments = vuln.find('COMMENTS').text
-                vuln_id = vuln[0][1].text
-                severity = vuln[1][1].text
-                title = vuln[5][1].text
-                description = vuln[6][1].text
-                ia_control = vuln[7][1].text
-                chk_content = vuln[8][1].text
-                fix_text = vuln[9][1].text
+            if root.tag == "CHECKLIST":
+                for vuln in root.findall('.//VULN'):
+                    status = vuln.find('STATUS').text
+                    comments = vuln.find('COMMENTS').text
+                    vuln_id = vuln[0][1].text
+                    severity = vuln[1][1].text
+                    title = vuln[5][1].text
+                    description = vuln[6][1].text
+                    ia_control = vuln[7][1].text
+                    chk_content = vuln[8][1].text
+                    fix_text = vuln[9][1].text
+                    try:
+                        vuln_id = VulnId(vuln_id=vuln_id)
+                        vuln_id.save()
+                    except IntegrityError:
+                        vuln_id = vuln[0][1].text
+                        vuln_id = VulnId.objects.get(vuln_id=vuln_id)
+                    print(vuln_id.vuln_id)
+                    if Weakness.objects.filter(system=System.objects.get(name=system), vuln_id=vuln_id).exists():
+                        Weakness.objects.filter(system=System.objects.get(name=system), vuln_id=vuln_id).update(comments=comments,
+                        raw_severity=severity, source_identifying_event=source_id, source_identifying_tool=source_tool, check_contents=chk_content, fix_text=
+                        fix_text, point_of_contact=PointOfContact.objects.get(name=poc), status=status)
+                    else:
+                        if status == 'Open':
+                            data_dict = {'title': title, 'description': description, 'status': status, 'comments': comments,
+                                         'raw_severity': severity, 'source_identifying_event': source_id,
+                                         'source_identifying_tool': source_tool, 'vuln_id': vuln_id.id, 'check_contents': chk_content,
+                                         'fix_text': fix_text, 'system': System.objects.get(name=system).id,
+                                         'point_of_contact': PointOfContact.objects.get(name=poc).id}
+                            form = DocumentModelForm(data_dict)
+                            form.save()
+                        else:
+                            continue
+            else:
+                messages.error(self.request, 'Wrong File Type, Zac!')
+                return redirect(reverse("poam:upload-artifact"))
 
-                vuln_id = VulnId(vuln_id=vuln_id)
-                vuln_id.save()
-
-                if status == 'Open':
-                    data_dict = {'title': title, 'description': description, 'status': status, 'comments': comments,
-                                 'raw_severity': severity, 'source_identifying_event': source_id,
-                                 'source_identifying_tool': source_tool, 'vuln_id': vuln_id.id, 'check_contents': chk_content,
-                                 'fix_text': fix_text, 'system': System.objects.get(name=system).id,
-                                 'point_of_contact': PointOfContact.objects.get(name=poc).id}
-                    form = DocumentModelForm(data_dict)
-                    print(form.errors)
-                    form.save()
-                # elif status == 'Closed' and vuln_id ==
-                else:
-                    continue
         messages.success(self.request, 'Artifacts Uploaded Successfully!')
         return redirect(reverse("accounts:dashboard"))
 
