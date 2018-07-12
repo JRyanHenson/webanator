8800c8cfd8fb5e0363f171e59dda0f7ca7cd8293
weas3ls <shabaneh.ahmad@gmail.com>
2018-07-12 10:39:14 -0600

    migrations

--- webanator/settings/prod.py
+++ webanator/settings/prod.py
@@ -22,3 +22,15 @@ DATABASES = {
         'PORT': os.environ['DB_PORT'],
     }
 }
+
+
+# Static and media files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
